cd975839b72622e55a0320b9e6f68f66f247b9a0
DoonPsdn <tiagojosecs@hotmail.com>
2016-11-11 16:52:04 +0000

    Copy Messagins

---
+++ DirectoryService/src/DataMessaging/ConfirmationMessage.java
@@ -0,0 +1,35 @@
+package DataMessaging;
+
+/*
+ * @author Daniel Moreira
+ * @author Hugo Santos
+ * @author Tiago Santos 
+ */
+
+public class ConfirmationMessage {
+
+    private String serverName;
+    private Boolean exists;
+    
+    public ConfirmationMessage(String serverName){
+        this.serverName = serverName;
+        this.exists = false;
+    }
+    
+    public String getServerName() {
+        return serverName;
+    }
+
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+
+    public Boolean serverExists() {
+        return exists;
+    }
+
+    public void setExists(Boolean exists) {
+        this.exists = exists;
+    }
+    
+}
