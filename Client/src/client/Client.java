cb9cc325a5d93d31b8cc816ba2a72b7d60575c5d
drmoreira <a21240321@alunos.isec.pt>
2016-11-11 15:56:44 +0000

    Cliente e Servidor

---
+++ Client/src/client/Client.java
@@ -0,0 +1,15 @@
+
+package client;
+
+/**
+ * @author Daniel Moreira
+ * @author Hugo Santos
+ * @author Tiago Santos 
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ Server/src/server/Server.java
@@ -0,0 +1,15 @@
+
+package server;
+
+/**
+ * @author Daniel Moreira
+ * @author Hugo Santos
+ * @author Tiago Santos 
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
