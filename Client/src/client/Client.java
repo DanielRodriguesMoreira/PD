3d460c247b49babf2ee684fffbe4cebd6645fa2f
Hugo Santos <hugo_afc5@hotmail.com>
2016-12-20 14:49:55 +0000

    Update DataAddresses
    Cliente a verificar se existe algum igual e pede outra vez um username

--- Client/src/client/Client.java
+++ Client/src/client/Client.java
@@ -15,6 +15,8 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Daniel Moreira
@@ -46,7 +48,7 @@ public class Client implements Constants {
         this.directoryServicePort = directoryServicePort;
     }    
 
-    public void sendMessageToServiceDirectory(String tipoPedidoAExecutar) {
+    public void sendMessageToServiceDirectory(ClientMessage messageToDirectory,String tipoPedidoAExecutar) {
         try {
             serverDirectoryAddr = InetAddress.getByName(this.directoryServiceIP);
             serverDirectoryPort = Integer.parseInt(this.directoryServicePort);   
@@ -55,10 +57,10 @@ public class Client implements Constants {
             bOut = new ByteArrayOutputStream();            
             out = new ObjectOutputStream(bOut);
 
-
             /*  Criar o datagramAddress para enviar para o Service directory    */
-            dataAddress = new DataAddress(this.username, null, -1, -1);
+            dataAddress = new DataAddress(username, null, -1, -1);
             message = new ClientMessage(dataAddress, null, null, tipoPedidoAExecutar, null, null, false);
+            
             out.writeObject(message);
             out.flush();
 
@@ -66,6 +68,28 @@ public class Client implements Constants {
             dataSocket.send(packet);
             System.out.println("<Client> Enviei mensagem\n");
 
+        } catch (SocketException ex) {
+            System.out.println("Erro ao criar o DatagramSocket\n");
+        } catch (IOException ex) {
+            System.out.println("Erro ao criar objecto serializado\n");
+        }
+    }
+
+    public String OnlineServerstoString() {
+        String teste = null;
+        if(OnlineServers != null)
+        {
+            for(int i = 0; i < OnlineServers.size(); i++) {
+                teste += this.OnlineServers.get(i).getName();
+            }
+        }
+        if(teste == null)
+            teste = "No Server Active\n";
+        return teste;
+    }
+    
+    public void receiveMessage() {
+        try {
             /*  Receber a resposta da directory service  */
             packet = new DatagramPacket(new byte[DATAGRAM_MAX_SIZE], DATAGRAM_MAX_SIZE);
             dataSocket.receive(packet);
@@ -75,35 +99,27 @@ public class Client implements Constants {
             /*  Ler a lista de servidores ligados   */
             System.out.println("<Client> Packet received");
             message = (ClientMessage) in.readObject();
+            
 
             //mandar para a thread o porto em condicoes
-            serverDirectoryPort = packet.getPort();
+            //serverDirectoryPort = packet.getPort();
 
-            if (!message.isExists()) {
-                Thread t1 = new ImAliveThread(dataSocket, serverDirectoryAddr, serverDirectoryPort, dataAddress);
-                t1.start();
-
-                /* Listar lista de sevidores activos */
-                for(int i = 0; i < message.getListServers().size(); i++) {
-                    this.OnlineServers.add(message.getDataAddress());
-                    //System.out.println(message.getListServers().get(i).getName());
-                }
-            }
-        } catch (SocketException ex) {
-            System.out.println("Erro ao criar o DatagramSocket\n");
+            /*if (!message.isExists()) {
+            Thread t1 = new ImAliveThread(dataSocket, serverDirectoryAddr, serverDirectoryPort, dataAddress);
+            t1.start();
+            
+            this.OnlineServers = message.getListServers();
+            }*/
         } catch (IOException ex) {
-            System.out.println("Erro ao criar objecto serializado\n");
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
-            System.out.println("Erro ao ler o objecto serialiazado\n");
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
-    public String OnlineServerstoString() {
-        String teste = null;
-        for(int i = 0; i < OnlineServers.size(); i++) {
-                    teste += this.OnlineServers.get(i).getName();
-                    System.out.println(this.OnlineServers.get(i).getName());
-                }
-        return teste;
+    
+    public boolean checkClientExists() {
+        sendMessageToServiceDirectory(message, CLIENT_MSG_CHECK_USERNAME);
+        receiveMessage();
+        return message.isExists();
     }
 }
