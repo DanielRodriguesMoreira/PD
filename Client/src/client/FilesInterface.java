90f6be3dcfb4496fc58e216e3ba4993073415024
drmoreira <a21240321@alunos.isec.pt>
2016-12-27 20:33:40 +0000

    add function to interface

--- Client/src/client/FilesInterface.java
+++ Client/src/client/FilesInterface.java
@@ -25,5 +25,7 @@ public interface FilesInterface {
             throws ServerConnectionException, UsernameOrPasswordIncorrectException, ClientNotLoggedInException, CreateAccountException;
     public File[] GetWorkingDirContent(String path, DataAddress serverToSend) 
             throws ServerConnectionException, UsernameOrPasswordIncorrectException, ClientNotLoggedInException, CreateAccountException;
+    public String GetWorkingDirPath(String path, DataAddress serverToSend) 
+            throws ServerConnectionException, UsernameOrPasswordIncorrectException, ClientNotLoggedInException, CreateAccountException;
     public boolean GetFilesInDirectory(File directory);
 }
